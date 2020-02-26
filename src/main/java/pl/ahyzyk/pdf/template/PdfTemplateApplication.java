2275b8a9f7b3f6e75889a36f667d1458bd58b684
ahyzyk <domus@konto.pl>
2020-02-26 17:14:08 +0100

    init commit

---
+++ src/main/java/pl/ahyzyk/pdf/template/PdfTemplateApplication.java
@@ -0,0 +1,13 @@
+package pl.ahyzyk.pdf.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfTemplateApplication.class, args);
+    }
+
+}
